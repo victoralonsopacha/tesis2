369325819e746c948b504da1a4a0f6c51b91a52b
victoralonso <victoralonsopacha@outlook.es>
2021-03-09 19:42:33 -0500

    formulario descarga pdf resumen final

--- resources/views/pdf/informes.blade.php
+++ resources/views/pdf/informes.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <title>Document</title>
     <title></title>
     <style>
@@ -17,27 +18,29 @@
 <h1>ESCUELA VELASCO IBARRA</h1>
 <h2>Reporte de timbradas de usuario TIMBRADAS BIOMETRICO</h2>
 
-    <table class="table">
-        <thead>
 
-        </thead>
-        <tbody>
-            <tr><td>{{{ $informe['cedula'] }}}</td></tr>
-            <tr><td>{{{ $informe['nombre'] }}}</td></tr>
-            <tr><td>{{{ $informe['totaltrabajado'] }}}</td></tr>
-            <tr><td>{{{ $informe['dias'] }}}</td></tr>
-            <tr><td>{{{ $informe['horas'] }}}</td></tr>
-            <tr><td>{{{ $informe['atrasos'] }}}</td></tr>
-            <tr><td>{{{ $informe['total'] }}}</td></tr>
-            <tr><td>{{{ $informe['permisos'] }}}</td></tr>
-            <tr><td>{{{ $informe['aprobados'] }}}</td></tr>
-            <tr><td>{{{ $informe['aprobados'] }}}</td></tr>
-            <tr><td>{{{ $informe['desaprobados'] }}}</td></tr>
-            <tr><td>{{{ $informe['pendientes'] }}}</td></tr>
-            <tr><td>{{{ $informe['reposicion'] }}}</td></tr>
 
-        </tbody>
-    </table>
+    
+
+
+    <form>
+    <P>RESUMEN DE HORAS TRABAJADAS</P>
+  <p>Cedula: <label for="">{{{ $informe['cedula'] }}}</label></p>
+  <p>Nombre: <label for="">{{{ $informe['nombre'] }}}</label></p>
+
+  <p>Horas totales trabajadas: <label for="">{{{ $informe['totaltrabajado'] }}}</label></p>
+
+  <p>Dias totales trabajados: <label for="">{{{ $informe['dias'] }}}</label></p>
+  <p>Atrasos: <label for="">{{{ $informe['atrasos'] }}}</label></p>
+  <p>PERMISOS</p>
+  <p>Horas totaes de permisos: <label for="">{{{ $informe['permisos'] }}}</label></p>
+  <p>Horas totaes de permisos: <label for="">{{{ $informe['aprobados'] }}}</label></p>
+  <p>Horas totaes de permisos: <label for="">{{{ $informe['desaprobados'] }}}</label></p>
+  <p>Horas totaes de permisos: <label for="">{{{ $informe['pendientes'] }}}</label></p>
+  <p>Horas totaes de permisos: <label for="">{{{ $informe['reposicion'] }}}</label></p>
+
+</form>
+    
 </body>
 </html>
 
