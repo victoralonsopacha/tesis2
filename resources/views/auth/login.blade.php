486090033c2bfe05e374bbec212e67f7ce2ce7a5
JazminVS <jazminv8.95@gmail.com>
2021-05-08 18:46:22 -0500

    cambios

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -56,7 +56,7 @@
                             <div class="form-group row mb-0">
                                 <div class="col-md-8 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
-                                        {{ __('Iniciar Sesion') }}
+                                        {{ __('Iniciar Sesión') }}
                                     </button>
 
                                     @if (Route::has('password.request'))
