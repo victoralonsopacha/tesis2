2332b268f9c33bfd3c886ae1bf09b1f2e4556ece
JazminVS <jazminv8.95@gmail.com>
2021-05-10 21:12:19 -0500

    Validacion fecha en crear reposicion - Profesor

--- resources/views/tiempo_reposicions/create.blade.php
+++ resources/views/tiempo_reposicions/create.blade.php
@@ -15,40 +15,40 @@
         @include('partials.validation-errors')
         <form method="POST" id="reposicion_atraso" action="{{ route('tiempo_reposicions.store') }}">
             @csrf
-
+            
             <div class="form-group row">
-                <label for="staticEmail" class="col-sm-2 col-form-label">Cédula del usuario</label>
-                <div class="col-sm-2">
+                <label for="staticEmail" class="col-sm-3 col-form-label">Cédula del usuario</label>
+                <div class="col-sm-9 col-md-6">
                     <input type="text" class="form-control" name="cedula" value="{{$cedula=auth()->user()->cedula}}" readonly>
                 </div>
             </div>
             <div class="form-group row">
-                <label for="staticEmail" class="col-sm-2 col-form-label">Nombre del usuario</label>
-                <div class="col-sm-2">
+                <label for="staticEmail" class="col-sm-3 col-form-label">Nombre del usuario</label>
+                <div class="col-sm-9 col-md-6">
                     <input type="text" class="form-control" name="nombre" value="{{$cedula=auth()->user()->name}}" readonly>
                 </div>
             </div>
             <div class="form-group row">
-                <label for="staticEmail" class="col-sm-2 col-form-label">Apellido del usuario</label>
-                <div class="col-sm-2">
+                <label for="staticEmail" class="col-sm-3 col-form-label">Apellido del usuario</label>
+                <div class="col-sm-9 col-md-6">
                     <input type="text" class="form-control" name="apellido" value="{{$cedula=auth()->user()->last_name}}" readonly>
                 </div>
             </div>
             <div class="form-group row">
-                <label for="staticEmail" class="col-sm-2 col-form-label">Cantidad de Tiempo</label>
-                <div class="col-sm-2">
-                    <input type="time" min="00:00" max="08:00" class="form-control" name="horas">
+                <label for="staticEmail" class="col-sm-3 col-form-label">Cantidad de Tiempo</label>
+                <div class="col-sm-9 col-md-6">
+                    <input type="time" min="00:00" max="08:00" class="form-control" name="horas" required>
                 </div>
             </div>
             <div class="form-group row">
-                <label for="staticEmail" class="col-sm-2 col-form-label">Día que desea reponer</label>
-                <div class="col-sm-2">
-                    <input type="date" class="form-control" id="fecha" name="fecha">
+                <label for="staticEmail" class="col-sm-3 col-form-label">Día que desea reponer</label>
+                <div class="col-sm-9 col-md-6">
+                    <input type="date" class="form-control" id="fecha" name="fecha" required>
                 </div>
             </div>
             <div class="form-group row">
-                <div class="col-sm-6">
-                    <button type="submit" class="btn btn-success">Solicitar</button>
+                <div class="col-md-10 col-sm-12 text-center">
+                    <button type="submit" class="btn btn-success">Aceptar</button>
                 </div>
             </div>
         </form>
